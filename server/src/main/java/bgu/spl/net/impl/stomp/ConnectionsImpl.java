54b739e2d93c84c590a35de581755080c4880915
IdoNaim <idonai@post.bgu.ac.il>
2025-01-19 20:51:01 +0200

    commented maybe unneeded implementation in ConnectionsImpl

--- server/src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
@@ -8,34 +8,34 @@ import bgu.spl.net.srv.Connections;
 
 public class ConnectionsImpl<T> implements Connections<T> {
 
-    private ConcurrentHashMap<Integer, ConnectionHandler<T>> activeUsers;
-    private ConcurrentHashMap<String, List<Integer>> subscribtions;
-    private int index;
-    public ConnectionsImpl(){
-        activeUsers = new ConcurrentHashMap<>();
-    }
-    public int connect(ConnectionHandler<T> ch){
-        activeUsers.putIfAbsent(index, ch);
-        index++;
-        return index-1;
-    }
+    // private ConcurrentHashMap<Integer, ConnectionHandler<T>> activeUsers;
+    // private ConcurrentHashMap<String, List<Integer>> subscribtions;
+    // private int index;
+    // public ConnectionsImpl(){
+        // activeUsers = new ConcurrentHashMap<>();
+    //}
+    // public int connect(ConnectionHandler<T> ch){
+    //     // activeUsers.putIfAbsent(index, ch);
+    //     // index++;
+    //     // return index-1;
+    // }
     public boolean send(int connectionId, T msg){
         //TODO: implement
-        ConnectionHandler<T> ch = activeUsers.get(connectionId);
-        ch.send(msg);
-        return true;
+        // ConnectionHandler<T> ch = activeUsers.get(connectionId);
+        // ch.send(msg);
+         return true;
     }
 
     public void send(String channel, T msg){
         //TODO: implement
-        List<Integer> list = subscribtions.get(channel);
-        for (Integer user : list) {
-            send(user, msg);
-        }
+        //List<Integer> list = subscribtions.get(channel);
+        // for (Integer user : list) {
+        //     send(user, msg);
+        // }
     }
 
     public void disconnect(int connectionId){
         //TODO: implement
-        activeUsers.remove(connectionId);
+        //activeUsers.remove(connectionId);
     }
 }
