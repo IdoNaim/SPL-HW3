4177d9d14185eeb430e94ab3d93387e1576e4545
IdoNaim <idonai@post.bgu.ac.il>
2025-01-21 18:15:22 +0200

    merged and started implementing connections

--- server/src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/ConnectionsImpl.java
@@ -8,12 +8,20 @@ import bgu.spl.net.srv.Connections;
 
 public class ConnectionsImpl<T> implements Connections<T> {
 
-    // private ConcurrentHashMap<Integer, ConnectionHandler<T>> activeUsers;
-    // private ConcurrentHashMap<String, List<Integer>> subscribtions;
-    // private int index;
-    // public ConnectionsImpl(){
-        // activeUsers = new ConcurrentHashMap<>();
-    //}
+    private ConcurrentHashMap<Integer, ConnectionHandler<T>> activeUsers;
+    private ConcurrentHashMap<String, List<Integer>> subscribtions;
+    private ConcurrentHashMap<String, String> userNameToPasscode;
+    private ConcurrentHashMap<Integer,ConcurrentHashMap<String, Integer>> connectionIdChannelToSubscribtionId;
+    private ConcurrentHashMap<String, Integer> userNameToConnectionId; //maybe unneeded
+    private int index;
+    public ConnectionsImpl(){
+        activeUsers = new ConcurrentHashMap<>();
+        subscribtions = new ConcurrentHashMap<>();
+        userNameToPasscode = new ConcurrentHashMap<>();
+        userNameToConnectionId = new ConcurrentHashMap<>();
+        connectionIdChannelToSubscribtionId = new ConcurrentHashMap<>();
+        index = 0; 
+    }
     // public int connect(ConnectionHandler<T> ch){
     //     // activeUsers.putIfAbsent(index, ch);
     //     // index++;
@@ -21,21 +29,51 @@ public class ConnectionsImpl<T> implements Connections<T> {
     // }
     public boolean send(int connectionId, T msg){
         //TODO: implement
-        // ConnectionHandler<T> ch = activeUsers.get(connectionId);
-        // ch.send(msg);
-         return true;
+        try{
+        ConnectionHandler<T> ch = activeUsers.get(connectionId);
+        ch.send(msg);
+        return true;
+        }catch(Exception e){
+            return false;
+        }
+
     }
 
     public void send(String channel, T msg){
         //TODO: implement
-        //List<Integer> list = subscribtions.get(channel);
-        // for (Integer user : list) {
-        //     send(user, msg);
-        // }
+        List<Integer> list = subscribtions.get(channel);
+        if(list != null){
+            for (Integer user : list) {
+                String stompMessage = 
+                "MESSAGE"+'\n'+
+                "subscribtion:" + connectionIdChannelToSubscribtionId.get(user).get(channel)+'\n'+
+                "message-id:"+ index +'\n'+
+                "destination:/"+channel+'\n'+
+                ""+'\n'+
+                msg+'\n'+
+                '\u0000';
+                send(user, msg);
+            }
+            index ++;
+        }else{
+//             String errorMsg=
+//                 "message: channerl doesnt exist"+'\n'+
+//                   ""+'\n'+
+//                      "The message:"+'\n'+
+//                      "----"+'\n'+
+//                      msg+'\n'+
+//                      "----"+'\n'+
+// ;
+//             send()
+        }
     }
 
     public void disconnect(int connectionId){
         //TODO: implement
         //activeUsers.remove(connectionId);
     }
+    public String createError(String body){
+
+    }
+
 }
