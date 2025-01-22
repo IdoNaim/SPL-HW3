52bcb9e0a96f8565078422626f01d511d0dfc24a
IdoNaim <idonai@post.bgu.ac.il>
2025-01-23 01:04:28 +0200

    changed lineMessageEncoderDecoder to reading a message when seeing \u0000 instead of \n

--- server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -1,5 +1,7 @@
 package bgu.spl.net.impl.stomp;
 
+import bgu.spl.net.impl.echo.EchoProtocol;
+import bgu.spl.net.impl.echo.LineMessageEncoderDecoder;
 import bgu.spl.net.srv.Server;
 
 public class StompServer {
@@ -7,7 +9,11 @@ public class StompServer {
     public static void main(String[] args) {
         //TODO: implement
         if (args[1].equals("tpc")){
-
+            Server.threadPerClient(
+                7777, //port
+                () -> new StompMessagingProtocolImpl(), //protocol factory
+                LineMessageEncoderDecoder::new //message encoder decoder factory
+        ).serve();
         }
         else if(args[1].equals("reactor")){
         
