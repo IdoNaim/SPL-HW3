8c936ad3805b8b27d9677d3566e594de7a257073
IdoNaim <idonai@post.bgu.ac.il>
2025-01-23 00:22:28 +0200

    added sending CONNECT frame on connecting

--- server/src/main/java/bgu/spl/net/impl/stomp/ConnectFrame.java
+++ server/src/main/java/bgu/spl/net/impl/stomp/ConnectFrame.java
@@ -10,14 +10,21 @@ public class ConnectFrame extends Frame {
     public void process(Connections<String> connections) {
         String userName = headers.get("login");
         String password = headers.get("passcode");
-        
+        String connectedMsg = 
+        "CONNECTED"+'\n'+
+        "version:"+ headers.get("accept-vesion")+'\n'+
+        ""+'\n'+
+        '\u0000';
         if(!connections.userExists(userName)){
             connections.registerUser(userName,password);
             connections.connect(userName, connectionId);
+            connections.send(connectionId, connectedMsg);
         }else{
             if(connections.userPassword(userName,password)){
                 if(!connections.isUserOnline(userName)){
                     connections.connect(userName, connectionId);
+                    connections.send(connectionId, connectedMsg);
+
                 }
                 else{
                     String errorMsg=
